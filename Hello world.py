7066d6eab2bd27e5d1bb98ea803e8ac581c26c4c
newrepo <mahender.saragada10@gmail.com>
2022-10-20 09:38:18 +0530

    my Python 1st files

---
+++ Hello world.py
@@ -0,0 +1,15 @@
+name = 'SMR'
+name1 = "john Smith"
+age = 20
+is_published =True
+is_published =F
+print(is_published)
+print(name1)
+print(age)
+print("3+4")
+print(3+4.5)
+print("3-4")
+print(3-4)
+print(3*4)
+print(3/4)
+
---
+++ user.py
@@ -0,0 +1,10 @@
+name = input('what is ur Name ? ')
+print('Hi Dr.'+ name)
+fav_color = input('What is Favourite Color ? ')
+print( name +' like ' + fav_color )
+birth_year = input('Birth Year: ')
+print(type(birth_year))
+age = 2019 -int(birth_year) # int(birth_year) string is converted into integer
+print(type(age))
+print(age)
+
